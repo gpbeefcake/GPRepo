a3b35efc7d34342a6863a8f641346cde5f7e37f1
gpbeefcake <panagiog427@icloud.com>
2017-03-13 13:42:47 +1100

    VMcreation
    HyperV

---
+++ PSScripts/NewVM.ps1
@@ -0,0 +1,17 @@
+﻿# Set VM Name, Switch Name, and Installation Media Path.
+$VMName = 'Burner10'
+$Switch = 'ExtSwitch'
+$InstallMedia = 'C:\NotBackedup\Installables\en_windows_10_multiple_editions_version_1607_updated_jan_2017_x64_dvd_9714399.iso'
+
+# Create New Virtual Machine
+New-VM -Name $VMName -MemoryStartupBytes 2147483648 -Generation 2 -NewVHDPath "C:\Virtual Machines\$VMName\$VMName.vhdx" -NewVHDSizeBytes 53687091200 -Path "C:\Virtual Machines\$VMName" -SwitchName $Switch
+
+# Add DVD Drive to Virtual Machine
+Add-VMScsiController -VMName $VMName
+Add-VMDvdDrive -VMName $VMName -ControllerNumber 1 -ControllerLocation 0 -Path $InstallMedia
+
+# Mount Installation Media
+$DVDDrive = Get-VMDvdDrive -VMName $VMName
+
+# Configure Virtual Machine to Boot from DVD
+Set-VMFirmware -VMName $VMName -FirstBootDevice $DVDDrive
